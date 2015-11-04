963b1ac1fcfe3de3ac848fb904371394ca06759a
josue chavez <chechejca@hotmail.com>
2015-11-03 20:29:50 -0700

    Added player info and cards array

--- js/main.js
+++ js/main.js
@@ -2,8 +2,17 @@ $( document ).ready(function() {
     var bigBlind, smallBlind, playerDealer, gameType, gameMode, roomName, timeOutHand;
     var players = [];
 
-    $('#start').on("click", function(){
+    // D = Diamonds, H = Hearts, C = Clovers, S = Spades
+    var cards = ['1d', '2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', '10d', '11d', '12d', '13d',
+                 '1h', '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', '10h', '11h', '12h', '13h',
+                 '1c', '2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', '10c', '11c', '12c', '13c',
+                 '1s', '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', '10s', '11s', '12s', '13s']
+
+    Math.floor((Math.random() * cards.length) + 1);
+
+    $('#sit').on("click", function(){
         $('#table').removeClass("no-display");
+        //$('#btnActions').removeClass("no-display");
         $('#players').addClass("no-display");
 
         $('#smallBlindInfo').text($('#smallBlind').val());
@@ -19,31 +28,43 @@ $( document ).ready(function() {
         $('#timeOutHand').addClass('no-display');
 
         if($('#name1').val()!='' && $('#initialStack1').val()!=''){
-            $('#player1').text($('#name1').val() + " Stack " + $('#initialStack1').val());
+            $('#name1Info').text($('#name1').val());
+            $('#initialStack1Info').text($('#initialStack1').val());
         }
         if($('#name2').val()!='' && $('#initialStack2').val()!=''){
-            $('#player2').text($('#name2').val() + " Stack " + $('#initialStack2').val());
+            $('#name2Info').text($('#name2').val());
+            $('#initialStack2Info').text($('#initialStack2').val());
         }
         if($('#name3').val()!='' && $('#initialStack3').val()!=''){
-            $('#player3').text($('#name3').val() + " Stack " + $('#initialStack3').val());
+            $('#name3Info').text($('#name3').val());
+            $('#initialStack3Info').text($('#initialStack3').val());
         }
         if($('#name4').val()!='' && $('#initialStack4').val()!=''){
-            $('#player4').text($('#name4').val() + " Stack " + $('#initialStack4').val());
+            $('#name4Info').text($('#name4').val());
+            $('#initialStack4Info').text($('#initialStack4').val());
         }
         if($('#name5').val()!='' && $('#initialStack5').val()!=''){
-            $('#player5').text($('#name5').val() + " Stack " + $('#initialStack5').val());
+            $('#name5Info').text($('#name5').val());
+            $('#initialStack5Info').text($('#initialStack5').val());
         }
         if($('#name6').val()!='' && $('#initialStack6').val()!=''){
-            $('#player6').text($('#name6').val() + " Stack " + $('#initialStack6').val());
+            $('#name6Info').text($('#name6').val());
+            $('#initialStack6Info').text($('#initialStack6').val());
         }
         if($('#name7').val()!='' && $('#initialStack7').val()!=''){
-            $('#player7').text($('#name7').val() + " Stack " + $('#initialStack7').val());
+            $('#name7Info').text($('#name7').val());
+            $('#initialStack7Info').text($('#initialStack7').val());
         }
         if($('#name8').val()!='' && $('#initialStack8').val()!=''){
-            $('#player8').text($('#name8').val() + " Stack " + $('#initialStack8').val());
+            $('#name8Info').text($('#name8').val());
+            $('#initialStack8Info').text($('#initialStack8').val());
         }
         if($('#name9').val()!='' && $('#initialStack9').val()!=''){
-            $('#player9').text($('#name9').val() + " Stack " + $('#initialStack9').val());
+            $('#name9Info').text($('#name9').val());
+            $('#initialStack9Info').text($('#initialStack9').val());
         }
+
+        $('#start').removeClass("no-display");
+        $('#sit').addClass("no-display");
     });
 });
